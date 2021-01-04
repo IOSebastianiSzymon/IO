1b175d3206e28242104863f891ba9a42ac69210f
IOSebastianiSzymon <72760144+IOSebastianiSzymon@users.noreply.github.com>
2021-01-04 13:39:20 +0100

    Add files via upload

---
+++ zmian_na_04.01.2021/IO_project/static/javascript/mainpage_sliders.js
@@ -0,0 +1,215 @@
+$(document).ready(function(){
+
+  $('.post-wrapper').slick({
+
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+  });
+});
+
+$(document).ready(function(){
+
+  $('.post-wrapper-action').slick({
+
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next-action'),
+    prevArrow: $('.prev-action'),
+  });
+});
+
+
+$(document).ready(function(){
+
+  $('.post-wrapper-horror').slick({
+
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next-horror'),
+    prevArrow: $('.prev-horror'),
+  });
+});
+$(document).ready(function(){
+
+  $('.post-wrapper-fantasy').slick({
+
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next-fantasy'),
+    prevArrow: $('.prev-fantasy'),
+  });
+});
+
+
+
+// world war z 
+function Annabelle_Creation(){
+  document.getElementById("Annabelle-Creation").classList.toggle("active");
+  
+}
+
+function World_War_Z(){
+  document.getElementById("World-War-Z").classList.toggle("active");
+}
+
+
+function John_Wick_three(){
+  document.getElementById("John-Wick-3").classList.toggle("active");
+}
+function It_dwo(){
+  document.getElementById("IT-2").classList.toggle("active");
+}
+function Avengers_four(){
+  document.getElementById("Avengers-4").classList.toggle("active");
+}
+function Annabelle_Comes_Home(){
+  document.getElementById("Annabelle-Comes-Home").classList.toggle("active");
+}
+function John_Wick_two(){
+  document.getElementById("John-Wick-2").classList.toggle("active");
+}
+function Avengers_three(){
+  document.getElementById("Avengers-3").classList.toggle("active");
+}
+function John_Wick_one(){
+  document.getElementById("John-Wick-1").classList.toggle("active");
+}
+
+function Avengers_one(){
+    document.getElementById("Avengers-1").classList.toggle("active");
+ }
+function Avengers_two(){
+  document.getElementById("Avengers-2").classList.toggle("active");
+}
+function It_one(){
+  document.getElementById("IT-1").classList.toggle("active");
+}
+function The_Witch(){
+  document.getElementById("The-Witch").classList.toggle("active");
+}
+
+function The_Silence_Of_The_Lambs(){
+  document.getElementById("The-Silence-Of-The-Lambs").classList.toggle("active");
+}
+
+function Harry_Potter_one(){
+  document.getElementById("Harry-Potter-1").classList.toggle("active");
+}
+
+
+function Hobbit_one(){
+  document.getElementById("Hobbit-1").classList.toggle("active");
+}
+
+function Fantastic_Beasts_one(){
+  document.getElementById("Fantastic-Beasts-1").classList.toggle("active");
+}
+
+function Harry_Potter_two(){
+  document.getElementById("Harry-Potter-2").classList.toggle("active");
+}
+
+function Bright(){
+  document.getElementById("Bright").classList.toggle("active");
+}
+
+function Star_Trek(){
+  document.getElementById("Star-Trek").classList.toggle("active");
+}
+
+function Black_Panther(){
+  document.getElementById("Black-Panther").classList.toggle("active");
+}
+
+
+// modal movies
+function World_War_Z_movie(){
+  document.getElementById("World-War-Z-movie").classList.toggle("active");
+}
+function Annabelle_Creation_movie(){
+  document.getElementById("Annabelle-Creation-movie").classList.toggle("active");
+}
+
+function John_Wick_three_movie(){
+  document.getElementById("John-Wick-3-movie").classList.toggle("active");
+}
+function It_dwo_movie(){
+  document.getElementById("IT-2-movie").classList.toggle("active");
+}
+function Avengers_four_movie(){
+  document.getElementById("Avengers-4-movie").classList.toggle("active");
+}
+
+function Annabelle_Comes_Home_movie(){
+  document.getElementById("Annabelle-Comes-Home-movie").classList.toggle("active");
+}
+
+function John_Wick_two_movie(){
+  document.getElementById("John-Wick-2-movie").classList.toggle("active");
+}
+
+function Avengers_three_movie(){
+  document.getElementById("Avengers-3-movie").classList.toggle("active");
+}
+function John_Wick_one_movie(){
+  document.getElementById("John-Wick-1-movie").classList.toggle("active");
+}
+
+function Avengers_one_movie(){
+  document.getElementById("Avengers-1-movie").classList.toggle("active");
+}
+
+function Avengers_two_movie(){
+  document.getElementById("Avengers-2-movie").classList.toggle("active");
+}
+
+function It_one_movie(){
+  document.getElementById("IT-1-movie").classList.toggle("active");
+}
+
+function The_Witch_movie(){
+  document.getElementById("The-Witch-movie").classList.toggle("active");
+}
+
+function The_Silence_Of_The_Lambs_movie(){
+  document.getElementById("The-Silence-Of-The-Lambs-movie").classList.toggle("active");
+}
+
+function Harry_Potter_one_movie(){
+  document.getElementById("Harry-Potter-1-movie").classList.toggle("active");
+}
+
+function Hobbit_one_movie(){
+  document.getElementById("Hobbit-1-movie").classList.toggle("active");
+}
+
+function Fantastic_Beasts_one_movie(){
+  document.getElementById("Fantastic-Beasts-1-movie").classList.toggle("active");
+}
+
+function Harry_Potter_two_movie(){
+  document.getElementById("Harry-Potter-2-movie").classList.toggle("active");
+}
+
+function Bright_movie(){
+  document.getElementById("Bright-movie").classList.toggle("active");
+}
+
+
+function Star_Trek_movie(){
+  document.getElementById("Star-Trek-movie").classList.toggle("active");
+}
+
+function Black_Panther_movie(){
+  document.getElementById("Black-Panther-movie").classList.toggle("active");
+}
